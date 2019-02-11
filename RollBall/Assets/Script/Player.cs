fc14724c324c908f073c333ca1eb43ed27fca10d
geunyoon <geunyoon7@gmail.com>
2019-02-11 05:44:39 -0500

    Allow player to collect the Collectable object and made Collectable into dynamic object

--- RollBall/Assets/Script/Player.cs
+++ RollBall/Assets/Script/Player.cs
@@ -18,7 +18,15 @@ public class Player : MonoBehaviour
         float moveVertical = Input.GetAxis ("Vertical");
         //putting the keyboard input
         Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
-
         rb.AddForce (movement * speed);
+        //adjusting speed of player
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Collectable"))
+        {
+            other.gameObject.SetActive(false);
+        }
     }
 }
