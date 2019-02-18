a5bda38147b57fa84225efe2437755b1866f93f3
geunyoon <geunyoon7@gmail.com>
2019-02-18 03:01:50 -0500

    Add the respawning feature when player fall off

---
+++ RollBall/Assets/Script/Respawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    [SerializeField]
+    private Transform player;
+    [SerializeField]
+    private Transform respawnPoint;
+    //referencing the respawn point and player
+
+    void OnTriggerEnter(Collider other)
+    {
+        player.transform.position = respawnPoint.transform.position;
+    }
+}
--- RollBall/Assets/Script/Timer.cs
+++ RollBall/Assets/Script/Timer.cs
@@ -6,17 +6,21 @@ using TMPro;
 public class Timer : MonoBehaviour
 {
     TextMeshProUGUI text;
+    //text field
 
-    public float time = 60;
+    public float time = 30;
+    //remaining time
 
     void Start()
     {
         text = GetComponent<TextMeshProUGUI>();
+        //initailization
     }
 
     void Update()
     {
         time -= Time.deltaTime;
         text.text = string.Format("{0:0}", time);
+        //counting the timer
     }
 }
